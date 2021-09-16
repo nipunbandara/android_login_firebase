7c2735df3119f55f16d101d1eeabcac83f93027e
nipunbandara <nipun.bandara@outlook.com>
2021-09-16 20:54:52 +0530

    Initial commit

---
+++ app/src/main/java/com/mad/login_test/MainActivity.java
@@ -0,0 +1,36 @@
+package com.mad.login_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener((View.OnClickListener) this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/mad/login_test/User.java
@@ -0,0 +1,16 @@
+package com.mad.login_test;
+
+public class User {
+
+    public String fullName, age, email;
+
+    public User(){
+
+    }
+
+    public User(String fullName, String age, String emial){
+        this.fullName = fullName;
+        this.age = age;
+        this.email = emial;
+    }
+}
